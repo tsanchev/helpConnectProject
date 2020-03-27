64c66a0a29bbb11d3ff3cc205a843e74b3f344eb
tsanchev <tsanchev@gmail.com>
2020-03-28 01:06:47 +0200

    initial commit

---
+++ helpconnect.jangu.space/index.php
@@ -0,0 +1,22 @@
+<?php
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
+
+
+$dir = '/../helpconnect';
+
+require(__DIR__ . $dir . '/vendor/autoload.php');
+require(__DIR__ . $dir . '/vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . $dir . '/common/config/bootstrap.php');
+require(__DIR__ . $dir . '/frontend/config/bootstrap.php');
+
+$config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . $dir . '/common/config/main.php')
+    ,require(__DIR__ . $dir . '/common/config/main-local.php')
+    ,require(__DIR__ . $dir . '/frontend/config/main.php')
+    ,require(__DIR__ . $dir . '/frontend/config/main-local.php')
+);
+
+(new yii\web\Application($config))->run();
+
